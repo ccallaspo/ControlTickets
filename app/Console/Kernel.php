53e2f603f2c1da56e4fbd6b129b9ba64d1fc58df
Cafru <cafutrille@gmail.com>
2024-12-27 17:43:33 -0300

    Tarea programada

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('report:daily-courses')->dailyAt('07:00');
     }
 
     /**
